de8a9594d9c62e21c3690702ffa97b9b2988c653
Neil Mayhew <neil.mayhew@iohk.io>
2026-04-09 12:18:56 -0600

    Add golden tests for `cabal`

--- test/Test/Cabal/Spec.hs
+++ test/Test/Cabal/Spec.hs
@@ -1,6 +1,16 @@
 module Test.Cabal.Spec where
 
+import System.Process (readProcess)
+import Test.Common.Fixture (fixturePath)
+import Test.Common.Golden (goldenTest)
 import Test.Hspec
 
+import qualified Data.Text as T
+
 spec :: Spec
-spec = pure ()
+spec = do
+  it "targets" $ do
+    projectDir <- fixturePath "Cabal"
+    expected <- fixturePath "Cabal/targets.golden"
+    actual <- T.pack <$> readProcess "cleret" ["cabal", "targets", "-p", projectDir] ""
+    goldenTest expected actual
