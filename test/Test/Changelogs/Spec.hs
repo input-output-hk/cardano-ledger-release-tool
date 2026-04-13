bdf78c83104f0de887894980ae5337f1a8abe5d3
Neil Mayhew <neil.mayhew@iohk.io>
2026-04-13 11:21:56 -0600

    Add golden tests for `changelogs`

--- test/Test/Changelogs/Spec.hs
+++ test/Test/Changelogs/Spec.hs
@@ -1,6 +1,25 @@
 module Test.Changelogs.Spec where
 
+import System.Exit (ExitCode (..))
+import System.FilePath ((<.>))
+import System.Process (readProcessWithExitCode)
+import Test.Common.Fixture (fixturePath)
+import Test.Common.Golden (goldenTest)
 import Test.Hspec
 
+import qualified Data.Text as T
+
 spec :: Spec
-spec = pure ()
+spec = do
+  specify "format" $ do
+    inputFile <- fixturePath "Changelogs/CHANGELOG.md"
+    let expected = inputFile <.> "golden"
+    (code, out, err) <- readProcessWithExitCode "cleret" ["changelogs", "format", inputFile] ""
+    err `shouldSatisfy` null
+    goldenTest expected (T.pack out)
+    code `shouldBe` ExitSuccess
+
+-- TODO:
+--  Markdown parsing failures
+--  Version parsing failures
+--  Unexpected structure
