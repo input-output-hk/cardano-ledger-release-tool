1a00975e54070ee904c949e180a8466ec83c4e80
Neil Mayhew <neil.mayhew@iohk.io>
2026-08-10 18:19:58 -0600

    Add a golden test for `cabal list-bins`

--- test/Test/Cabal/Spec.hs
+++ test/Test/Cabal/Spec.hs
@@ -9,8 +9,13 @@ import qualified Data.Text as T
 
 spec :: Spec
 spec = do
-  it "targets" $ do
+  specify "targets" $ do
     projectDir <- fixturePath "Cabal"
     expected <- fixturePath "Cabal/targets.golden"
     actual <- T.pack <$> readProcess "cleret" ["cabal", "targets", "-p", projectDir] ""
+    goldenTest expected actual
+  specify "list-bins" $ do
+    projectDir <- fixturePath "Cabal"
+    expected <- fixturePath "Cabal/list-bins.golden"
+    actual <- T.pack <$> readProcess "cleret" ["cabal", "list-bins", "-p", projectDir] ""
     goldenTest expected actual
