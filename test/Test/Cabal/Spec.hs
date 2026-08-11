89f2e92d955efc9df8e13f68233347639156f67b
Neil Mayhew <neil.mayhew@iohk.io>
2026-08-11 09:08:08 -0600

    Add a golden test for `cabal relativize-plan`

--- test/Test/Cabal/Spec.hs
+++ test/Test/Cabal/Spec.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Test.Cabal.Spec where
 
-import System.Process (readProcess)
+import Cabal.Plan (SearchPlanJson (ProjectRelativeToDir), findPlanJson)
+import System.FilePath (takeFileName, (</>))
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (callProcess, readProcess)
 import Test.Common.Fixture (fixturePath)
 import Test.Common.Golden (goldenTest)
 import Test.Hspec
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 spec :: Spec
 spec = do
@@ -19,3 +25,20 @@ spec = do
     expected <- fixturePath "Cabal/list-bins.golden"
     actual <- T.pack <$> readProcess "cleret" ["cabal", "list-bins", "-p", projectDir] ""
     goldenTest expected actual
+  specify "relativize-plan" $ do
+    projectFixture <- fixturePath "Cabal"
+    expected <- fixturePath "Cabal/relativize-plan.golden"
+    withTempProject projectFixture $ \projectDir -> do
+      planFile <- findPlanJson $ ProjectRelativeToDir projectDir
+      let modifyFile fp f = T.writeFile fp . f =<< T.readFile fp
+      modifyFile planFile $ T.replace "dist-newstyle" (T.pack $ projectDir </> "dist-newstyle")
+      callProcess "cleret" ["cabal", "relativize-plan", "-p", projectDir]
+      goldenTest expected =<< T.readFile planFile
+
+withTempProject :: FilePath -> (FilePath -> IO ()) -> IO ()
+withTempProject projectFixture action = do
+  withSystemTempDirectory "cleret-workdir" $ \workdir -> do
+    let projectDir = workdir </> takeFileName projectFixture
+    callProcess "cp" ["-r", projectFixture, projectDir]
+    callProcess "chmod" ["-R", "u+wx", projectDir] -- Needed for nix
+    action projectDir
