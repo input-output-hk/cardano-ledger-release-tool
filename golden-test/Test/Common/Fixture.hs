c10d148e76bb84f47531efd72cdbefe5a63553a5
Neil Mayhew <neil.mayhew@iohk.io>
2026-08-12 08:12:27 -0600

    Rename the golden tests subdirectory

--- golden-test/Test/Common/Fixture.hs
+++ golden-test/Test/Common/Fixture.hs
@@ -4,4 +4,4 @@ import Paths_cardano_ledger_release_tool (getDataFileName)
 import System.FilePath ((</>))
 
 fixturePath :: FilePath -> IO FilePath
-fixturePath relPath = getDataFileName $ "test/fixtures" </> relPath
+fixturePath relPath = getDataFileName $ "golden-test/fixtures" </> relPath
